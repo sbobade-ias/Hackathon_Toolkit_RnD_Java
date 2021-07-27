cb792e1beaaf986767909ed01af15d34d7175a86
sbobade-ias <83100546+sbobade-ias@users.noreply.github.com>
2021-07-27 18:23:12 +0530

    Hackathon- User API improvement

--- src/main/java/com/toolkit/app/dao/UserDAO.java
+++ src/main/java/com/toolkit/app/dao/UserDAO.java
@@ -3,8 +3,25 @@ package com.toolkit.app.dao;
 public class UserDAO {
 
     private int id;
-    private String firstName;
-    private String LastName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getChapter() {
+        return chapter;
+    }
+
+    public void setChapter(String chapter) {
+        this.chapter = chapter;
+    }
+
+    private String chapter;
+    private String userName;
     private String designation;
     private String phone;
     private String address1;
@@ -13,6 +30,107 @@ public class UserDAO {
     private String state;
     private String country;
     private String role;
+
+    private String department;
+    private String team;
+    private String manager;
+    private String total_exp;
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getManager() {
+        return manager;
+    }
+
+    public void setManager(String manager) {
+        this.manager = manager;
+    }
+
+    public String getTotal_exp() {
+        return total_exp;
+    }
+
+    public void setTotal_exp(String total_exp) {
+        this.total_exp = total_exp;
+    }
+
+    public String getRelavent_exp() {
+        return relavent_exp;
+    }
+
+    public void setRelavent_exp(String relavent_exp) {
+        this.relavent_exp = relavent_exp;
+    }
+
+    public String getCertifications() {
+        return certifications;
+    }
+
+    public void setCertifications(String certifications) {
+        this.certifications = certifications;
+    }
+
+    public String getIas_clients() {
+        return ias_clients;
+    }
+
+    public void setIas_clients(String ias_clients) {
+        this.ias_clients = ias_clients;
+    }
+
+    public String getProjects() {
+        return projects;
+    }
+
+    public void setProjects(String projects) {
+        this.projects = projects;
+    }
+
+    public String getAdtech_exp() {
+        return adtech_exp;
+    }
+
+    public void setAdtech_exp(String adtech_exp) {
+        this.adtech_exp = adtech_exp;
+    }
+
+    public String getAdtech_poc() {
+        return adtech_poc;
+    }
+
+    public void setAdtech_poc(String adtech_poc) {
+        this.adtech_poc = adtech_poc;
+    }
+
+    public String getAspired_tech() {
+        return aspired_tech;
+    }
+
+    public void setAspired_tech(String aspired_tech) {
+        this.aspired_tech = aspired_tech;
+    }
+
+    private String relavent_exp;
+    private String certifications;
+    private String ias_clients;
+    private String projects;
+    private String adtech_exp;
+    private String adtech_poc;
+    private String aspired_tech;
 
     public String getDesignation() {
         return designation;
@@ -86,22 +204,6 @@ public class UserDAO {
         this.id = id;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-
-    public String getLastName() {
-        return LastName;
-    }
-
-    public void setLastName(String lastName) {
-        LastName = lastName;
-    }
 
 
 }
--- src/main/java/com/toolkit/app/repository/UserRepository.java
+++ src/main/java/com/toolkit/app/repository/UserRepository.java
@@ -18,6 +18,12 @@ public class UserRepository {
 
     final String SELECT_BY_ID_QUERY = "SELECT * from Users where id = ?";
     final String SELECT_ALL_QUERY = "SELECT * from Users";
+    final String SELECT_LOGIN_QUERY = "SELECT * from users,User_profile " +
+            "where users.id=user_profile.id "+
+            "and username = ? and password = ? and role = ?";
+    final String SELECT_USER_DETAILS = "select * from users,User_profile " +
+            "where users.id=user_profile.id " +
+            "and users.id = ?";
 
 
     public UserDAO findUsers(int userId) {
@@ -29,11 +35,41 @@ public class UserRepository {
         return jdbcTemplate.query(SELECT_ALL_QUERY, new UserMapper());
     }
 
+    public List<UserDAO> login(String username, String password, String  role)
+    {
+       List<UserDAO> user = jdbcTemplate.query(SELECT_LOGIN_QUERY,
+               new Object[] {username,password,role},new UserMapper());
+       return user;
+    }
+
+    private static final class LoginMapper implements RowMapper{
+        public UserDAO mapRow(ResultSet rs, int rowNum) throws SQLException {
+            UserDAO user = new UserDAO();
+            user.setUserName((rs.getString("UserName")));
+            user.setId((rs.getInt("ID")));
+            user.setDesignation(rs.getString("Designation"));
+            return user;
+        }
+    }
+
     private static final class UserMapper implements RowMapper<UserDAO> {
         public UserDAO mapRow(ResultSet rs, int rowNum) throws SQLException {
             UserDAO user = new UserDAO();
-            user.setFirstName(rs.getString("FIRSTNAME"));
-            user.setLastName(rs.getString("LASTNAME"));
+            user.setUserName((rs.getString("UserName")));
+            user.setId((rs.getInt("ID")));
+            user.setDesignation(rs.getString("Designation"));
+            user.setDepartment(rs.getString("department"));
+            user.setTeam(rs.getString("team"));
+            user.setChapter(rs.getString("chapter"));
+            user.setManager(rs.getString("manager"));
+            user.setTotal_exp(rs.getString("total_exp"));
+            user.setRelavent_exp(rs.getString("relavent_exp"));
+            user.setCertifications(rs.getString("certifications"));
+            user.setIas_clients(rs.getString("ias_clients"));
+            user.setProjects(rs.getString("projects"));
+            user.setAdtech_exp(rs.getString("adtech_exp"));
+            user.setAdtech_poc(rs.getString("adtech_poc"));
+            user.setAspired_tech(rs.getString("aspired_tech"));
             return user;
         }
     }
