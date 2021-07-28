0591ca7d0e44af0e3fd7606ac60105bdff4f6ea8
ndeepak <ndeepak@integralads.com>
2021-07-28 06:36:54 +0530

    User skills API

--- src/main/java/com/toolkit/app/controller/UserController.java
+++ src/main/java/com/toolkit/app/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.toolkit.app.controller;
 
 import com.toolkit.app.dao.UserDAO;
+import com.toolkit.app.dao.UserSkillsDAO;
 import com.toolkit.app.service.UserService;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,30 +19,40 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @RequestMapping(value="/insert")
-    public String index(){
+    @RequestMapping(value = "/insert")
+    public String index() {
         jdbc.execute("insert into test(name,email)values('javatpoint','java@javatpoint.com')");
-        return"data inserted Successfully";
+        return "data inserted Successfully";
     }
 
     @RequestMapping(value = "/test")
-    public String test(){
+    public String test() {
         System.out.println("Testkjdakdj......");
-        return"Welcome ..";
+        return "Welcome ..";
     }
 
-    @GetMapping(value ="/getUsers")
-            public List<UserDAO> getUsers(){
+    @GetMapping(value = "/getUsers")
+    public List<UserDAO> getUsers() {
         List<UserDAO> list = userService.getUsers();
         return list;
     }
 
     @RequestMapping(path = "/login/{username}/{password}/{role}", method = RequestMethod.GET)
-         public List<UserDAO> login(@PathVariable String username, @PathVariable String password, @PathVariable String  role){
+    public List<UserDAO> login(@PathVariable String username, @PathVariable String password, @PathVariable String role) {
         List<UserDAO> user = userService.login(username, password, role);
         return user;
     }
 
+    @RequestMapping(path = "/userSkills/{userId}", method = RequestMethod.GET)
+    public UserSkillsDAO getUserSkills(@PathVariable String userId) {
+        UserSkillsDAO userSkills = userService.getUserSkills(userId);
+        return userSkills;
+    }
 
-
+    //LIMIT TO 3 SECONDARY SKILLS
+    @RequestMapping(path = "/userSkills/update", method = RequestMethod.POST)
+    public HttpStatus setUserSkills(@RequestBody UserSkillsDAO userSkills) {
+        userService.setUserSkills(userSkills);
+        return HttpStatus.ACCEPTED;
+    }
 }
---
+++ src/main/java/com/toolkit/app/dao/UserSkillsDAO.java
@@ -0,0 +1,244 @@
+package com.toolkit.app.dao;
+
+public class UserSkillsDAO {
+
+    private int id;
+
+    private int userId;
+
+    private String primarySkill;
+
+    private int primarySkillRanking;
+
+    private String secondarySkill1;
+
+    private int secondarySkill1Ranking;
+
+    private String secondarySkill2;
+
+    private int secondarySkill2Ranking;
+
+    private String secondarySkill3;
+
+    private int secondarySkill3Ranking;
+
+    private String secondarySkill4;
+
+    private int secondarySkill4Ranking;
+
+    private String secondarySkill5;
+
+    private int secondarySkill5Ranking;
+
+    private String secondarySkill6;
+
+    private int secondarySkill6Ranking;
+
+    private String secondarySkill7;
+
+    private int secondarySkill7Ranking;
+
+    private String secondarySkill8;
+
+    private int secondarySkill8Ranking;
+
+    private String secondarySkill9;
+
+    private int secondarySkill9Ranking;
+
+    private String secondarySkill10;
+
+    private int secondarySkill10Ranking;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getPrimarySkill() {
+        return primarySkill;
+    }
+
+    public void setPrimarySkill(String primarySkill) {
+        this.primarySkill = primarySkill;
+    }
+
+    public int getPrimarySkillRanking() {
+        return primarySkillRanking;
+    }
+
+    public void setPrimarySkillRanking(int primarySkillRanking) {
+        this.primarySkillRanking = primarySkillRanking;
+    }
+
+    public String getSecondarySkill1() {
+        return secondarySkill1;
+    }
+
+    public void setSecondarySkill1(String secondarySkill1) {
+        this.secondarySkill1 = secondarySkill1;
+    }
+
+    public int getSecondarySkill1Ranking() {
+        return secondarySkill1Ranking;
+    }
+
+    public void setSecondarySkill1Ranking(int secondarySkill1Ranking) {
+        this.secondarySkill1Ranking = secondarySkill1Ranking;
+    }
+
+    public String getSecondarySkill2() {
+        return secondarySkill2;
+    }
+
+    public void setSecondarySkill2(String secondarySkill2) {
+        this.secondarySkill2 = secondarySkill2;
+    }
+
+    public int getSecondarySkill2Ranking() {
+        return secondarySkill2Ranking;
+    }
+
+    public void setSecondarySkill2Ranking(int secondarySkill2Ranking) {
+        this.secondarySkill2Ranking = secondarySkill2Ranking;
+    }
+
+    public String getSecondarySkill3() {
+        return secondarySkill3;
+    }
+
+    public void setSecondarySkill3(String secondarySkill3) {
+        this.secondarySkill3 = secondarySkill3;
+    }
+
+    public int getSecondarySkill3Ranking() {
+        return secondarySkill3Ranking;
+    }
+
+    public void setSecondarySkill3Ranking(int secondarySkill3Ranking) {
+        this.secondarySkill3Ranking = secondarySkill3Ranking;
+    }
+
+    public String getSecondarySkill4() {
+        return secondarySkill4;
+    }
+
+    public void setSecondarySkill4(String secondarySkill4) {
+        this.secondarySkill4 = secondarySkill4;
+    }
+
+    public int getSecondarySkill4Ranking() {
+        return secondarySkill4Ranking;
+    }
+
+    public void setSecondarySkill4Ranking(int secondarySkill4Ranking) {
+        this.secondarySkill4Ranking = secondarySkill4Ranking;
+    }
+
+    public String getSecondarySkill5() {
+        return secondarySkill5;
+    }
+
+    public void setSecondarySkill5(String secondarySkill5) {
+        this.secondarySkill5 = secondarySkill5;
+    }
+
+    public int getSecondarySkill5Ranking() {
+        return secondarySkill5Ranking;
+    }
+
+    public void setSecondarySkill5Ranking(int secondarySkill5Ranking) {
+        this.secondarySkill5Ranking = secondarySkill5Ranking;
+    }
+
+    public String getSecondarySkill6() {
+        return secondarySkill6;
+    }
+
+    public void setSecondarySkill6(String secondarySkill6) {
+        this.secondarySkill6 = secondarySkill6;
+    }
+
+    public int getSecondarySkill6Ranking() {
+        return secondarySkill6Ranking;
+    }
+
+    public void setSecondarySkill6Ranking(int secondarySkill6Ranking) {
+        this.secondarySkill6Ranking = secondarySkill6Ranking;
+    }
+
+    public String getSecondarySkill7() {
+        return secondarySkill7;
+    }
+
+    public void setSecondarySkill7(String secondarySkill7) {
+        this.secondarySkill7 = secondarySkill7;
+    }
+
+    public int getSecondarySkill7Ranking() {
+        return secondarySkill7Ranking;
+    }
+
+    public void setSecondarySkill7Ranking(int secondarySkill7Ranking) {
+        this.secondarySkill7Ranking = secondarySkill7Ranking;
+    }
+
+    public String getSecondarySkill8() {
+        return secondarySkill8;
+    }
+
+    public void setSecondarySkill8(String secondarySkill8) {
+        this.secondarySkill8 = secondarySkill8;
+    }
+
+    public int getSecondarySkill8Ranking() {
+        return secondarySkill8Ranking;
+    }
+
+    public void setSecondarySkill8Ranking(int secondarySkill8Ranking) {
+        this.secondarySkill8Ranking = secondarySkill8Ranking;
+    }
+
+    public String getSecondarySkill9() {
+        return secondarySkill9;
+    }
+
+    public void setSecondarySkill9(String secondarySkill9) {
+        this.secondarySkill9 = secondarySkill9;
+    }
+
+    public int getSecondarySkill9Ranking() {
+        return secondarySkill9Ranking;
+    }
+
+    public void setSecondarySkill9Ranking(int secondarySkill9Ranking) {
+        this.secondarySkill9Ranking = secondarySkill9Ranking;
+    }
+
+    public String getSecondarySkill10() {
+        return secondarySkill10;
+    }
+
+    public void setSecondarySkill10(String secondarySkill10) {
+        this.secondarySkill10 = secondarySkill10;
+    }
+
+    public int getSecondarySkill10Ranking() {
+        return secondarySkill10Ranking;
+    }
+
+    public void setSecondarySkill10Ranking(int secondarySkill10Ranking) {
+        this.secondarySkill10Ranking = secondarySkill10Ranking;
+    }
+}
